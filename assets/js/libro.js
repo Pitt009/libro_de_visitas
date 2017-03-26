4982e0810aedaa945936bc14c9447b9674ddbd55
Pitt009 <gmartinez28837@gmail.com>
2017-03-26 08:55:31 -0600

    libro de visitas

---
+++ assets/js/libro.js
@@ -0,0 +1,9 @@
+function mostrarTexto() {
+  var texto = document.getElementById('textoarea').value;
+  document.getElementById('textoAbajo').innerHTML = texto;
+}
+
+function textoGrande() {
+
+  document.getElementById('textoAbajo').font.size
+}
